271c818d2d75e1d469d72ba05eee471f1159c43b
jpammerlaan <jpammerlaan@gmail.com>
2015-07-18 21:11:00 +0200

    Update from laptop

--- public/js/include/controllers.js
+++ public/js/include/controllers.js
@@ -3,10 +3,27 @@
 (function() {
     var app = angular.module('app.controllers', []);
 
-    app.controller('UserCtrl', function() {
+    app.controller('HomeCtrl', function() {
+        var self = this;
+    });
+
+    app.controller('ProfileCtrl', function() {
+        var self = this;
+    });
+
+    app.controller('StatsCtrl', function() {
+        var self = this;
+    })
+
+    app.controller('SettingsCtrl', function() {
+        var self = this;
+    });
+
+    app.controller('UserCtrl', ['$scope', function($scope) {
         var self = this;
 
-        self.user = {};
+        console.log($scope.user);
+        $scope.user = {};
 
         self.isGuest = function() {
            return self.user.guest;
@@ -17,15 +34,21 @@
         };
 
         self.logIn = function() {
-            self.user = {
+            console.log($scope.user);
+            $scope.user = {
                 admin: true,
                 guest: false
             };
+            console.log($scope.user);
         };
 
         self.logOut = function() {
             // LOG USER OUT
-            self.user = null;
+            console.log('x');
+            console.log($scope.user);
+            $scope.user = null;
+            console.log($scope.user);
         };
-    });
+
+    }]);
 })();
